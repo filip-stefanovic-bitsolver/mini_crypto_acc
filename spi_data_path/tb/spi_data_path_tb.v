0185d5e64c71ffa1b4e1a0d933586127e86000f1
AleksandarCurcin <120393033+AleksandarCurcin@users.noreply.github.com>
2023-01-18 12:20:24 +0100

    Update spi_data_path_tb.v

--- spi_data_path/tb/spi_data_path_tb.v
+++ spi_data_path/tb/spi_data_path_tb.v
@@ -21,40 +21,78 @@ reg miso_start;
 
 
 
-
 initial 
   begin
     clk = 1;
     sclk = 1;
     reset_n = 0;
     cs_n = 1;
-    spi_mode = 2'b10;
-    mosi = 4'b0001;
-    rdata = 16'hC69A;
-    
-    #50ns;
-    reset_n = 1;
-    	
-    #10ns;
-    cs_n = 0;
-
-   // #1930ns
-    //mosi = 4'b0000;
-    //spi_mode = 2'b11;
-
-    #1930ns
-    mosi = 4'b0000;
-
-    #5880ns;
-    cs_n = 1;
-
-    
-       
-    #10us;
-    $finish;
-       
+    task_mod();  
+    #30us;
+    $finish;     
 end
 
+task task_mod();
+  begin
+    task_mod_1 (reset_n, cs_n, spi_mode, mosi, rdata);
+    task_mod_2 (reset_n, cs_n, spi_mode, mosi, rdata);
+    task_mod_4 (reset_n, cs_n, spi_mode, mosi, rdata);
+  end
+endtask
+
+task task_mod_1;
+  begin
+    spi_mode = 2'b01;
+    mosi = 4'b0001;
+    rdata = 16'hC69A;
+    #50ns;
+    reset_n = 1;
+    #10ns;
+    cs_n = 0;
+    #1930ns
+    mosi = 4'b0000;
+    #1930ns
+    mosi = 4'b0000;
+    #5880ns;
+    cs_n = 1;
+  end
+endtask  
+
+  task task_mod_2;
+  begin
+    spi_mode = 2'b10;
+    mosi = 4'b0001;
+    rdata = 16'h8BFA;
+    #50ns;
+    reset_n = 1;
+    #10ns;
+    cs_n = 0;
+    #1930ns
+    mosi = 4'b0000;
+    #1930ns
+    mosi = 4'b0000;
+    #5880ns;
+    cs_n = 1;
+  end
+endtask  
+
+  task task_mod_4;
+  begin
+    spi_mode = 2'b11;
+    mosi = 4'b0001;
+    rdata = 16'hC69A;
+    #50ns;
+    reset_n = 1;
+    #10ns;
+    cs_n = 0;
+    #1930ns
+    mosi = 4'b0000;
+    #1930ns
+    mosi = 4'b0000;
+    #5880ns;
+    cs_n = 1;
+  end
+endtask  
 
 initial 
   begin
@@ -62,27 +100,22 @@ initial
     $dumpvars;
   end
 
-
 always 
   begin
     #5ns;
     clk = ~clk;
   end
 
-
-always 
+  always 
   begin
     #20ns;
     if(~cs_n)
     begin
     sclk = ~sclk;
     mosi[0] = ~mosi[0];
-    
     end
-    //#10us;
-    //$finish;
   end
-
+  
 spi_data_path spi_data_path (clk,
                             reset_n,
                             sclk,
